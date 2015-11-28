e63287117d77b1b0178cd94a489a8657f32cf9f6
Keita Okamoto <ok@aaa.email.ne.jp>
2015-11-28 20:00:53 +0900

    Rename to OKBlock from Emblem.

--- test/index.test.js
+++ test/index.test.js
@@ -3,6 +3,6 @@ import '@all-user/ok-patterns-lines'
 import '@all-user/ok-patterns-olympic2020'
 
 // test
-import './Emblem.test.js'
-import './EmblemGroup.test.js'
+import './OKBlock.test.js'
+import './OKBlocksGroup.test.js'
 import './Lines.test.js'
