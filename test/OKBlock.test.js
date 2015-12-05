e12346d5338f050e523a7d8fc122e65a4daa55cc
Keita Okamoto <ok@aaa.email.ne.jp>
2015-12-06 02:34:46 +0900

    Update test.

--- test/OKBlock.test.js
+++ test/OKBlock.test.js
@@ -12,45 +12,45 @@ describe('OKBlock', () => {
     const EMBLEM_SIZE       = 90;
 
     describe('大文字小文字を区別しない', () => {
-        let runCases = [
-            {desc: '大文字で初期化', args: [BASE_CHAR_UPPER], result: BASE_CHAR_LOWER},
-            {desc: '小文字で初期化', args: [BASE_CHAR_LOWER], result: BASE_CHAR_LOWER}
+        let testCasess = [
+            { desc: '大文字で初期化', args: BASE_CHAR_UPPER, result: BASE_CHAR_LOWER },
+            { desc: '小文字で初期化', args: BASE_CHAR_LOWER, result: BASE_CHAR_LOWER }
         ];
 
-        runCases.forEach((runCase, i) => {
-            it(runCase.desc, done => {
-                let okblock = new OKBlock(...runCase.args, { pattern: 'Lines' });
-                assert.equal(okblock.char, runCase.result);
+        testCasess.forEach((testCases, i) => {
+            it(testCases.desc, done => {
+                let okblock = new OKBlock(testCases.args, { pattern: 'Lines' });
+                assert.equal(okblock.char, testCases.result);
                 done();
             });
         });
     });
 
     describe('instanceof OKBlock', () => {
-        let runCases = [
-            {desc: 'lower', args: [BASE_CHAR_LOWER], result: OKBlock},
-            {desc: 'null',  args: [null],            result: OKBlock}
+        let testCasess = [
+            { desc: '小文字で初期化', args: BASE_CHAR_LOWER, result: OKBlock },
+            { desc: 'nullで初期化',  args: null,            result: OKBlock }
         ];
 
-        runCases.forEach((runCase, i) => {
-            it(runCase.desc, done => {
-                let okblock = new OKBlock(...runCase.args, { pattern: 'Lines' });
-                assert.ok(okblock instanceof runCase.result);
+        testCasess.forEach((testCases, i) => {
+            it(testCases.desc, done => {
+                let okblock = new OKBlock(testCases.args, { pattern: 'Lines' });
+                assert.ok(okblock instanceof testCases.result);
                 done();
             });
         });
     });
 
     describe('OKBlock#char', () => {
-        let runCases = [
-            {desc: '小文字で初期化', args: [BASE_CHAR_LOWER], result: BASE_CHAR_LOWER},
-            {desc: 'nullで初期化',  args: [null],            result: null}
+        let testCasess = [
+            { desc: '小文字で初期化', args: BASE_CHAR_LOWER, result: BASE_CHAR_LOWER },
+            { desc: 'nullで初期化',  args: null,            result: null }
         ];
 
-        runCases.forEach((runCase, i) => {
-            it(runCase.desc, done => {
-                let okblock = new OKBlock(...runCase.args, { pattern: 'Lines' });
-                assert.equal(okblock.char, runCase.result);
+        testCasess.forEach((testCases, i) => {
+            it(testCases.desc, done => {
+                let okblock = new OKBlock(testCases.args, { pattern: 'Lines' });
+                assert.equal(okblock.char, testCases.result);
                 done();
             });
         });
@@ -58,67 +58,95 @@ describe('OKBlock', () => {
 
     describe('OKBlock#to', () => {
         describe('OKBlock#charは常に小文字', () => {
-            let runCases = [
-                { desc: '小文字で初期化、引数に小文字', args: [BASE_CHAR_LOWER, BASE_CHAR_LOWER], result: BASE_CHAR_LOWER },
-                { desc: '小文字で初期化、引数に大文字', args: [BASE_CHAR_LOWER, BASE_CHAR_UPPER], result: BASE_CHAR_LOWER },
-                { desc: 'nullで初期化、引数に小文字',  args: [null,            BASE_CHAR_LOWER], result: BASE_CHAR_LOWER },
-                { desc: 'nullで初期化、引数に大文字',  args: [null,            BASE_CHAR_UPPER], result: BASE_CHAR_LOWER }
+            let testCasess = [
+                {
+                    desc: '小文字で初期化、引数に小文字',
+                    args: {
+                        from: BASE_CHAR_LOWER,
+                        to:   BASE_CHAR_LOWER
+                    },
+                    result: BASE_CHAR_LOWER
+                },
+                {
+                    desc: '小文字で初期化、引数に大文字',
+                    args: {
+                        from: BASE_CHAR_LOWER,
+                        to:   BASE_CHAR_UPPER
+                    },
+                    result: BASE_CHAR_LOWER
+                },
+                {
+                    desc: 'nullで初期化、引数に小文字',
+                    args: {
+                        from: null,
+                        to:   BASE_CHAR_LOWER
+                    },
+                    result: BASE_CHAR_LOWER
+                },
+                {
+                    desc: 'nullで初期化、引数に大文字',
+                    args: {
+                        from: null,
+                        to:   BASE_CHAR_UPPER
+                    },
+                    result: BASE_CHAR_LOWER
+                }
             ];
 
-            runCases.forEach((runCase, i) => {
-                it(runCase.desc, done => {
-                    let okblock = new OKBlock(runCase.args[0], { pattern: 'Lines' });
-                    let res = okblock.to(runCase.args[1]);
-                    assert.equal(okblock.char, runCase.result[0]);
+            testCasess.forEach((testCases, i) => {
+                it(testCases.desc, done => {
+                    let okblock = new OKBlock(testCases.args.from, { pattern: 'Lines' });
+                    let res = okblock.to(testCases.args.to);
+                    assert.equal(okblock.char, testCases.result);
                     done();
                 });
             });
         });
 
         describe('OKBlock#pedal有効時、現在の文字と同じ文字を与えるとfalseを返す', () => {
-            let runCases = [
-                {desc: '小文字で初期化', args: [BASE_CHAR_LOWER], result: false},
-                {desc: 'nullで初期化',  args: [null],            result: false}
+            let testCasess = [
+                { desc: '小文字で初期化', args: BASE_CHAR_LOWER, result: false },
+                { desc: 'nullで初期化',  args: null,            result: false }
             ];
 
-            runCases.forEach((runCase, i) => {
-                it(runCase.desc, done => {
-                    let okblock = new OKBlock(...runCase.args, { pattern: 'Lines' });
+            testCasess.forEach((testCases, i) => {
+                it(testCases.desc, done => {
+                    let okblock = new OKBlock(testCases.args, { pattern: 'Lines' });
                     okblock.to(BASE_CHAR_LOWER);
                     let res = okblock.to(BASE_CHAR_LOWER);
-                    assert.equal(res, runCase.result);
+                    assert.equal(res, testCases.result);
                     done();
                 });
             });
         });
         
         describe('OKBlock#無効な文字を与えるとfalseを返す', () => {
-            let runCases = [
-                {desc: '小文字で初期化', args: [BASE_CHAR_LOWER], result: false},
-                {desc: 'nullで初期化',  args: [null],            result: false}
+            let testCasess = [
+                { desc: '小文字で初期化', args: BASE_CHAR_LOWER, result: false },
+                { desc: 'nullで初期化',  args: null,            result: false }
             ];
 
-            runCases.forEach((runCase, i) => {
-                it(runCase.desc, done => {
-                    let okblock = new OKBlock(...runCase.args, { pattern: 'Lines' });
+            testCasess.forEach((testCases, i) => {
+                it(testCases.desc, done => {
+                    let okblock = new OKBlock(testCases.args, { pattern: 'Lines' });
                     let res = okblock.to(BASE_CHAR_INVALID);
-                    assert.equal(res, runCase.result);
+                    assert.equal(res, testCases.result);
                     done();
                 });
             });
         });
         
         describe('OKBlock#無効な文字を与えてもOKBlock#charは変化しない', () => {
-            let runCases = [
-                {desc: '小文字で初期化', args: [BASE_CHAR_LOWER], result: null},
-                {desc: 'nullで初期化',  args: [null],            result: null}
+            let testCasess = [
+                { desc: '小文字で初期化', args: BASE_CHAR_LOWER, result: null },
+                { desc: 'nullで初期化',  args: null,            result: null }
             ];
 
-            runCases.forEach((runCase, i) => {
-                it(runCase.desc, done => {
-                    let okblock = new OKBlock(...runCase.args, { pattern: 'Lines' });
+            testCasess.forEach((testCases, i) => {
+                it(testCases.desc, done => {
+                    let okblock = new OKBlock(testCases.args, { pattern: 'Lines' });
                     okblock.to(BASE_CHAR_INVALID);
-                    assert.equal(okblock.char, runCase.args[0]);
+                    assert.equal(okblock.char, testCases.args);
                     done();
                 });
             });
@@ -136,26 +164,23 @@ describe('OKBlock', () => {
             pedal:       false,
         };
 
-        let runCases = [
-            {desc: '小文字で初期化', args: [BASE_CHAR_LOWER], result: opt},
-            {desc: 'nullで初期化',  args: [null],            result: opt}
+        let testCasess = [
+            { desc: '小文字で初期化', args: BASE_CHAR_LOWER, result: opt },
+            { desc: 'nullで初期化',  args: null,            result: opt }
         ];
 
-        runCases.forEach((runCase, i) => {
-            it(`${ runCase.desc }: 個別のパラメータ`, done => {
-                let okblock = new OKBlock(...runCase.args, { pattern: 'Lines' })
+        testCasess.forEach((testCases, i) => {
+            Object.keys(opt).forEach(paramName => {
+                let okblock = new OKBlock(testCases.args, { pattern: 'Lines' })
                 okblock.options = opt;
-                assert.equal(okblock.displayTime, opt.displayTime);
-                assert.equal(okblock.duration,    opt.duration);
-                assert.equal(okblock.easing,      opt.easing);
-                assert.equal(okblock.loop,        opt.loop);
-                assert.equal(okblock.random,      opt.random);
-                assert.equal(okblock.pedal,       opt.pedal);
-                done();
+                it(`${ testCases.desc }: ${ paramName }が正しく設定されている`, done => {
+                    assert.equal(okblock[paramName], opt[paramName]);
+                    done();
+                });
             });
 
-            it(`${ runCase.desc }: OKBlock#optionsが返すオブジェクト`, done => {
-                let okblock = new OKBlock(...runCase.args, { pattern: 'Lines' })
+            it(`${ testCases.desc }: OKBlock#optionsが正しいオブジェクトを返す`, done => {
+                let okblock = new OKBlock(testCases.args, { pattern: 'Lines' })
                 okblock.options = opt;
                 assert.deepEqual(okblock.options, opt);
                 done();
--- test/OKBlocksGroup.test.js
+++ test/OKBlocksGroup.test.js
@@ -6,164 +6,155 @@ describe('OKBlocksGroup test', () => {
     const LONG_COPY    = 'long copy ................';
     const SHORT_COPY   = 'short';
     const BLANK_COPY   = '                                                        ';
+    
+    const argsVariation = {
+        onlyPattern:        [TITLE_COPY, { pattern: 'Lines' }],
+        containLongLength:  [TITLE_COPY, { pattern: 'Lines', length: LONG_COPY.length }],
+        containShortLength: [TITLE_COPY, { pattern: 'Lines', length: SHORT_COPY.length }]
+    }
 
     describe('インスタンスの生成', () => {
-        let group = new OKBlocksGroup(TITLE_COPY, { pattern: 'Lines' });
-
-        it('文字列から生成', done => {
-            assert.equal(group.toString(), TITLE_COPY);
-            done();
-        });
-
-        it('長い文字列を与えて変換', done => {
-            group.map(LONG_COPY);
-            assert.equal(group.toString(), LONG_COPY.slice(0, TITLE_COPY.length));
-            done();
-        });
-
-        it('短い文字列を与えて変換', done => {
-            group.map(SHORT_COPY);
-            assert.equal(group.toString(), (SHORT_COPY + BLANK_COPY).slice(0, TITLE_COPY.length));
-            done();
-        });
-
-        describe('emblemGroup.optionsにオブジェクトを渡して設定', () => {
-            let opt = {
-                length:      40,
-                size:        800,
-                displayTime: 3000,
-                duration:    1200,
-                easing:      'cubic-bezier(.1,.8,.5,.9)',
-                loop:        false,
-                random:      true,
-                pedal:       false,
-            };
-
-            it('オプションの各パラメータが正しく設定されているか', done => {
-                group.options = opt;
-                assert.equal(group.length,      opt.length);
-                assert.equal(group.displayTime, opt.displayTime);
-                assert.equal(group.loop,        opt.loop);
-                assert.equal(group.random,      opt.random);
-
-                assert.deepEqual(group.size,     group.emblems.map(emb => emb.size));
-                assert.deepEqual(group.duration, group.emblems.map(emb => emb.duration));
-                assert.deepEqual(group.easing,   group.emblems.map(emb => emb.easing));
-                assert.deepEqual(group.pedal,    group.emblems.map(emb => emb.pedal));
+        let testCasess = [
+            {
+                desc: 'patternを指定して初期化',
+                args: argsVariation.onlyPattern,
+                result: TITLE_COPY
+            },
+            {
+                desc: 'pattern、lengthを指定、与える文字列より長いlengthで初期化',
+                args: argsVariation.containLongLength,
+                result: (TITLE_COPY + BLANK_COPY).slice(0, LONG_COPY.length)
+            },
+            {
+                desc: 'pattern、lengthを指定、与える文字列より短いlengthで初期化',
+                args: argsVariation.containShortLength,
+                result: TITLE_COPY.slice(0, SHORT_COPY.length)
+            },
+        ];
+        
+        testCasess.forEach(testCases => {
+            it(`${ testCases.desc }: 文字列から生成`, done => {
+                let group = new OKBlocksGroup(...testCases.args);
+                assert.equal(group.toString(), testCases.result);
                 done();
             });
-
-            it('group.optionsが返すオブジェクトが正しいか', done => {
-                let obj = {
-                    length:      opt.length,
-                    displayTime: opt.displayTime,
-                    loop:        opt.loop,
-                    random:      opt.random,
-                    size:        Array.from({ length: opt.length }, () => opt.size),
-                    duration:    Array.from({ length: opt.length }, () => opt.duration),
-                    easing:      Array.from({ length: opt.length }, () => opt.easing),
-                    pedal:       Array.from({ length: opt.length }, () => opt.pedal),
-                };
-                assert.deepEqual(group.options, obj);
+        });
+    });
+    
+    describe('OKBlocksGroup#map', () => {
+        let testCasess = [
+            {
+                desc: 'patternを指定して初期化: より長い文字列を引数にする',
+                args: {
+                    params: argsVariation.onlyPattern,
+                    copy: LONG_COPY
+                },
+                result: LONG_COPY.slice(0, argsVariation.onlyPattern[0].length)
+            },
+            {
+                desc: 'patternを指定して初期化: より短い文字列を引数にする',
+                args: {
+                    params: argsVariation.onlyPattern,
+                    copy: SHORT_COPY
+                },
+                result: (SHORT_COPY + BLANK_COPY).slice(0, argsVariation.onlyPattern[0].length)
+            },
+            {
+                desc: 'pattern、lengthを指定、与える文字列より長いlengthで初期化: より長い文字列を引数にする',
+                args: {
+                    params: argsVariation.containLongLength,
+                    copy: SHORT_COPY + LONG_COPY
+                },
+                result: (SHORT_COPY + LONG_COPY).slice(0, argsVariation.containLongLength[1].length)
+            },
+            {
+                desc: 'pattern、lengthを指定、与える文字列より長いlengthで初期化: より短い文字列を引数にする',
+                args: {
+                    params: argsVariation.containLongLength,
+                    copy: SHORT_COPY
+                },
+                result: (SHORT_COPY + BLANK_COPY).slice(0, argsVariation.containLongLength[1].length)
+            },
+            {
+                desc: 'pattern、lengthを指定、与える文字列より短いlengthで初期化: より長い文字列を引数にする',
+                args: {
+                    params: argsVariation.containShortLength,
+                    copy: LONG_COPY
+                },
+                result: LONG_COPY.slice(0, argsVariation.containShortLength[1].length)
+            },
+            {
+                desc: 'pattern、lengthを指定、与える文字列より短いlengthで初期化: より短い文字列を引数にする',
+                args: {
+                    params: argsVariation.containShortLength,
+                    copy: SHORT_COPY.slice(0, SHORT_COPY.length / 2 | 0)
+                },
+                result: (SHORT_COPY.slice(0, SHORT_COPY.length / 2 | 0) + BLANK_COPY).slice(0, argsVariation.containShortLength[1].length)
+            }
+        ];
+        
+        testCasess.forEach(testCases => {
+            it(`${ testCases.desc }`, done => {
+                let group = new OKBlocksGroup(...testCases.args.params);
+                group.map(testCases.args.copy);
+                assert.equal(group.toString(), testCases.result);
                 done();
             });
         });
     });
 
-    describe('長さを指定してインスタンスの生成', () => {
+    describe('OKBlocksGroup.optionsにパラメータオブジェクトを渡して設定', () => {
+        let testCases = [
+            {
+                desc: 'patternを指定して初期化',
+                args: argsVariation.onlyPattern
+            },
+            {
+                desc: 'pattern、lengthを指定、与える文字列より長いlengthで初期化',
+                args: argsVariation.containLongLength
+            },
+            {
+                desc: 'pattern、lengthを指定、与える文字列より短いlengthで初期化',
+                args: argsVariation.containShortLength
+            }
+        ];
+        
+        let opt = {
+            length:      40,
+            size:        800,
+            displayTime: 3000,
+            duration:    1200,
+            easing:      'cubic-bezier(.1,.8,.5,.9)',
+            loop:        false,
+            random:      true,
+            pedal:       false,
+        };
+        
+        let singleValueParamNames = ['length', 'displayTime', 'loop', 'random'];
+        let returnArrayParamNames = ['size', 'duration', 'easing', 'pedal'];
 
-        describe('与える文字列より長い長さを指定', () => {
-            let group = new OKBlocksGroup(TITLE_COPY, { pattern: 'Lines', length: LONG_COPY.length });
-
-            it('文字列から生成', done => {
-                assert.equal(group.toString(), (TITLE_COPY + BLANK_COPY).slice(0, LONG_COPY.length));
-                done();
+        testCases.forEach(testCase => {
+            singleValueParamNames.forEach(paramName => {
+                it(`${ testCase.desc }: Retrun single value: ${ paramName }が正しく設定されているか`, done => {
+                    let group = new OKBlocksGroup(...argsVariation.onlyPattern);
+                    group.options = opt;
+                    assert.equal(group[paramName], opt[paramName]);
+                    done();
+                });
             });
 
-            it('長い文字列を与えて変換', done => {
-                group.map(LONG_COPY);
-                assert.equal(group.toString(), LONG_COPY);
-                done();
+            returnArrayParamNames.forEach(paramName => {
+                it(`${ testCase.desc }: Retrun Array: ${ paramName }が正しく設定されているか`, done => {
+                    let group = new OKBlocksGroup(...argsVariation.onlyPattern);
+                    group.options = opt;
+                    assert.deepEqual(group[paramName], group.emblems.map(e => e[paramName]));
+                    done();
+                });
             });
 
-            it('短い文字列を与えて変換', done => {
-                group.map(SHORT_COPY);
-                assert.equal(group.toString(), (SHORT_COPY + BLANK_COPY).slice(0, LONG_COPY.length));
-                done();
-            });
-        })
-
-        describe('与える文字列より短い長さを指定', () => {
-            let group = new OKBlocksGroup(TITLE_COPY, { pattern: 'Lines', length: SHORT_COPY.length });
-
-            it('文字列から生成', done => {
-                assert.equal(group.toString(), TITLE_COPY.slice(0, SHORT_COPY.length));
-                done();
-            });
-
-            it('長い文字列を与えて変換', done => {
-                group.map(LONG_COPY);
-                assert.equal(group.toString(), LONG_COPY.slice(0, SHORT_COPY.length));
-                done();
-            });
-
-            it('短い文字列を与えて変換', done => {
-                group.map(SHORT_COPY);
-                assert.equal(group.toString(), SHORT_COPY);
-                done();
-            });
-        })
-
-        describe('サイズを指定', () => {
-            let group = new OKBlocksGroup(TITLE_COPY, { pattern: 'Lines', length: SHORT_COPY.length });
-
-            it('文字列から生成', done => {
-                assert.equal(group.toString(), TITLE_COPY.slice(0, SHORT_COPY.length));
-                done();
-            });
-
-            it('長い文字列を与えて変換', done => {
-                group.map(LONG_COPY);
-                assert.equal(group.toString(), LONG_COPY.slice(0, SHORT_COPY.length));
-                done();
-            });
-
-            it('短い文字列を与えて変換', done => {
-                group.map(SHORT_COPY);
-                assert.equal(group.toString(), SHORT_COPY);
-                done();
-            });
-
-        })
-
-        describe('emblemGroup.optionsにオブジェクトを渡して設定', () => {
-            let group = new OKBlocksGroup(TITLE_COPY, { pattern: 'Lines', length: SHORT_COPY.length });
-            let opt = {
-                length:      40,
-                size:        800,
-                displayTime: 3000,
-                duration:    1200,
-                easing:      'cubic-bezier(.1,.8,.5,.9)',
-                loop:        false,
-                random:      true,
-                pedal:       false,
-            };
-
-            it('オプションの各パラメータが正しく設定されているか', done => {
+            it(`${ testCase.desc }: group.optionsが正しいオブジェクトを返す`, done => {
+                let group = new OKBlocksGroup(...argsVariation.onlyPattern);
                 group.options = opt;
-                assert.equal(group.length,      opt.length);
-                assert.equal(group.displayTime, opt.displayTime);
-                assert.equal(group.loop,        opt.loop);
-                assert.equal(group.random,      opt.random);
-
-                assert.deepEqual(group.size,     group.emblems.map(emb => emb.size));
-                assert.deepEqual(group.duration, group.emblems.map(emb => emb.duration));
-                assert.deepEqual(group.easing,   group.emblems.map(emb => emb.easing));
-                assert.deepEqual(group.pedal,    group.emblems.map(emb => emb.pedal));
-                done();
-            });
-
-            it('group.optionsが返すオブジェクトが正しいか', done => {
                 let obj = {
                     length:      opt.length,
                     displayTime: opt.displayTime,
@@ -178,6 +169,5 @@ describe('OKBlocksGroup test', () => {
                 done();
             });
         });
-
     });
 });
