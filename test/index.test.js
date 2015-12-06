255a31256ad5b3c32ab6d759cb9f241de244211a
Keita Okamoto <ok@aaa.email.ne.jp>
2015-12-06 23:17:19 +0900

    Remove tests for styles of patterns from this repository.

--- test/index.test.js
+++ test/index.test.js
@@ -3,6 +3,5 @@ import '@all-user/ok-patterns-lines';
 import '@all-user/ok-patterns-olympic2020';
 
 // test
-import './Lines.test.js'
 import './OKBlock.test.js';
 import './OKBlocksGroup.test.js';
